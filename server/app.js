d0c24538980097bc2e49fdfbc35123dbb34b621f
Anon Karytski <heyheyjude23@gmail.com>
2021-03-10 02:27:58 +0300

    deploy settings commit

--- server/app.js
+++ server/app.js
@@ -9,12 +9,11 @@ app.use('/api/country', require('./routes/countries.routes'))
 
 if(process.env.NODE_ENV === 'production'){
     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    app.get('/', (req, res) => { res.send('Hello from Express!')})
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
-
-app.get('/', (req, res) => { res.send('Hello from Express!')})
 
 const PORT = process.env.PORT || config.get('port') || 5000
 
