a2202b68a0dbf0315bafdd3cdfafa88945815524
Anon Karytski <heyheyjude23@gmail.com>
2021-03-10 01:56:07 +0300

    deploy settings commit

--- server/app.js
+++ server/app.js
@@ -3,20 +3,16 @@ const config = require('config')
 const mongoose = require('mongoose')
 
 const app = express()
-
-
 app.use(express.json({extended : true}))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/country', require('./routes/countries.routes'))
 
-
-
-if(process.env.NODE_ENV === 'production'){
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if(process.env.NODE_ENV === 'production'){
+//     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
 const PORT = process.env.PORT || config.get('port') || 5000
 
