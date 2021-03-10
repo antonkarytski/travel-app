2ba592ceb7e688686f33b254be915f829d86d9e3
Anon Karytski <heyheyjude23@gmail.com>
2021-03-10 19:50:24 +0300

    deployment to gh-pages

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const application = (
-    <BrowserRouter basename={"/travel-app/"}>
+    <BrowserRouter basename={"/"}>
         <App />
     </BrowserRouter>
 )
